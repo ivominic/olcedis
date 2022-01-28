b8ea0427b4fc2112a3323c69a3b5e352c4cc060a
Ivo <ivo.minic@gmail.com>
2022-01-28 10:41:32 +0100

    Izmjena fajla za pojedine metode

--- js/edit/import_web_services.js
+++ js/edit/import_web_services.js
@@ -67,3 +67,52 @@ function objectsFinalInsert(objects, methodName) {
 
   return retval;
 }
+
+/**
+ * Method for inserting all drawn objects into database
+ * @param {*} stubovi
+ * @param {*} vodovi
+ * @param {*} trafostanice
+ * @param {*} podovi
+ * @param {*} prikljucna_mjesta
+ * @param {*} potrosaci
+ * @param {*} nkro
+ */
+function insertAllObjects(stubovi, vodovi, trafostanice, podovi, prikljucna_mjesta, potrosaci, nkro) {
+  let retval = true;
+  let urlServisa = wsServerOriginLocation + "/novi_portal/api/object_control";
+  console.log("stubovi insert all objects   ", JSON.stringify(stubovi));
+  console.log("vodovi insert all objects   ", JSON.stringify(vodovi));
+  console.log("trafostanice insert all objects   ", JSON.stringify(trafostanice));
+  console.log("podovi insert all objects   ", JSON.stringify(podovi));
+  console.log("prikljucna_mjesta insert all objects   ", JSON.stringify(prikljucna_mjesta));
+  console.log("potrosaci insert all objects   ", JSON.stringify(potrosaci));
+  console.log("nkro insert all objects   ", JSON.stringify(nkro));
+  $.ajax({
+    url: urlServisa,
+    data: {
+      temp_stubovi: JSON.stringify(stubovi),
+      temp_vodovi: JSON.stringify(vodovi),
+      temp_trafostanice: JSON.stringify(trafostanice),
+      temp_pod: JSON.stringify(podovi),
+      temp_prikljucno_mjesto: JSON.stringify(prikljucna_mjesta),
+      temp_potrosac: JSON.stringify(potrosaci),
+      temp_nkro: JSON.stringify(nkro),
+    },
+    type: "POST",
+    success: function (data) {
+      console.log("success insert all objects", data);
+      //Web services for insertion of data
+      objectsFinalInsert(stubovi, "stubovi_store");
+      objectsFinalInsert(vodovi, "vodovi_store");
+      objectsFinalInsert(trafostanice, "trafostanice_store");
+      objectsFinalInsert(podovi, "pod_store");
+      objectsFinalInsert(prikljucna_mjesta, "prikljucno_mjesto_store");
+      objectsFinalInsert(potrosaci, "potrosaci_store");
+      objectsFinalInsert(nkro, "nkro_store");
+    },
+    error: function (x, y, z) {
+      console.log("error insert all objects", x.responseText);
+    },
+  });
+}
