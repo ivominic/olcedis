e88bb9cd98d256376b2014cd154a9e0fc959b495
Ivo <ivo.minic@gmail.com>
2022-04-08 14:40:02 +0200

    Započet modul za dodavanje novih objekata u postojeći vod

---
+++ js/modify/vodDodavanjeObjekta.js
@@ -0,0 +1,5 @@
+// Modul za dodavanje novog čvora postojećem vodu
+//TODO: Upload fajla sa novim objektom, dodaju se atributi i prikazuje forma za odabir susjednih čvorova voda: #odabirCvorovaVodaDiv
+//TODO: Odabir čvorova istog voda
+//TODO: Ako je novi objekat stub, mijenja se geometrija. Ako je bilo koji drugi objekat, vod se dijeli i definiše se nadređemni i podređeni vod
+//TODO: Ovo treba uraditi na serverskoj strani, da bi znali koji dio sadrži nadređeni, a koji podređeni objekat.
