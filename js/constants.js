2bd8f479bfcef560b1cd7db85a87b0684294cf9f
Ivo Minic <ivo.minic@gmail.com>
2022-10-16 10:32:12 +0200

    Dodao novi fajl koji će sadržati globalne konstante

---
+++ js/constants.js
@@ -0,0 +1,18 @@
+/** Modul koji sadrži sve poruke i konstante koje se koriste u projektu */
+
+const NaponskiNivo = {
+  String04kV: "0.4",
+  String10kV: "10",
+  String35kV: "35",
+  Numeric04kV: 0.4,
+  Numeric10kV: 10,
+  Numeric35kV: 35,
+};
+
+const WizardPoruke = {
+  Uspjeh: "Uspješno izvršena akcija",
+};
+
+const UnosPoruke = {
+  Uspjeh: "Uspješno izvršena akcija",
+};
